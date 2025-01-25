76438a47f0389bb4e93787b1a9b56c4a0e2c61c1
Kerem Ağıröz <keremagiroz@gmail.com>
2025-01-25 23:11:10 +0300

    Animasyon ve logo eklendi

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,76 @@
+// açılış ekranımız buraya.
+import 'package:dotlottie_loader/dotlottie_loader.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/core/routes.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 127, 115, 2),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Logo ve yükleme göstergesi bölümü
+          Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                // Logo bölümü
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: Image.asset(
+                    'assets/images/amblem.webp',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                // // Yükleme ikonu
+                // const Icon(
+                //   CupertinoIcons.arrow_2_circlepath,
+                //   size: 40,
+                //   color: Colors.white,
+                // ),
+
+                const SizedBox(height: 20),
+
+                // // Yükleniyor yazısı
+                // const Text(
+                //   'Yükleniyor...',
+                //   style: TextStyle(
+                //     fontSize: 18,
+                //     fontWeight: FontWeight.w500,
+                //   ),
+                // ),
+                InkWell(
+                  onTap: () => context.go("/home"),
+                  child: SizedBox(
+                    width: 300,
+                    child: DotLottieLoader.fromAsset(
+                        "assets/motions/loading.lottie",
+                        frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
+                      if (dotlottie != null) {
+                        return Lottie.memory(
+                            dotlottie.animations.values.single);
+                      } else {
+                        return Container();
+                      }
+                    }),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
